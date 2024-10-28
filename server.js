a4ae315904fb52bf250820b0e3c51131568d892c
Abhinay kushwaha <abhinaythresia@gmail.com>
2024-10-28 17:00:25 +0530

    Atlas

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/mydatabase')
-
+mongoose.connect('mongodb+srv://ossabhinay:2nxWKTWuWiflOTtM@cluster0.0yf1l.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
